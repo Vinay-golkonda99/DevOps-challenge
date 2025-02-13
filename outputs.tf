07623e93e51e7da8df2847131b60849078a6d62c
Vinay-golkonda99 <122544244+Vinay-golkonda99@users.noreply.github.com>
2025-02-12 23:52:32 -0500

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the created EC2 instance"
-  value       = aws_instance.my_ec2.id
-}
-
-output "public_ip" {
-  description = "Public IP address of the instance"
-  value       = aws_instance.my_ec2.public_ip
-}
